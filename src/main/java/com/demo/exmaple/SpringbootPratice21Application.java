374e0632e1f2323d87ec9ccf044347e009742f10
diwaHa <diwahar008@gmail.com>
2023-10-04 12:33:28 +0530

    feat : new programe for health check\

---
+++ src/main/java/com/demo/exmaple/SpringbootPratice21Application.java
@@ -0,0 +1,13 @@
+package com.demo.exmaple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPratice21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootPratice21Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/exmaple/SpringbootPratice21ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.exmaple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPratice21ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
